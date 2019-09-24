18fdcb325936c0f14be70182fcb20c9984ad7e68
Tim Su <tim.su@adgeek.com.tw>
2019-09-24 19:05:13 +0800

    add logger folder

--- ai_optimizer/app_log/facebook_externals.sh
+++
@@ -1,5 +0,0 @@
-
-_now=$(date +"%m_%d_%Y")
-_facebook_external_py="/home/tim_su/ai_optimizer/opt/ai_optimizer/ai_optimizer/codes/facebook_externals.py"
-_facebook_external_log="/home/tim_su/ai_optimizer/opt/ai_optimizer/app_log/facebook_externals_folder/$_now.log"
-python3 $_facebook_external_py  >> $_facebook_external_log
