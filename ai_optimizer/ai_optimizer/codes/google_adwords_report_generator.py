56664bc30a21ce713998756a645c26d097113951
tim.su <tim.su@adgeek.com.tw>
2019-12-11 17:40:56 +0800

    [GDN][GSN] feel soooo good after adding breakdown features on report generator

--- ai_optimizer/ai_optimizer/codes/google_adwords_report_generator.py
+++ ai_optimizer/ai_optimizer/codes/google_adwords_report_generator.py
@@ -87,6 +87,9 @@ class ReportGenerator(object):
         'First page CPC', 'Max. CPM', 'Max. CPV', 'Max. CPC', 'Default max. CPC', 'Target CPA', 'Cost', 'Avg. Cost', 'Avg. CPC',
         'Cost / conv.', 'Cost / all conv.', 'Amount', 'AverageCost', 'AverageCpc', 'CostPerAllConversion', 'CpcBid'
     ]
+    param_types = {
+        'breakdowns': ['hour', 'day', 'device']
+    }
     
     def __init__(self, campaign_id, media):
         self.campaign_id = campaign_id
@@ -95,6 +98,7 @@ class ReportGenerator(object):
         self.report_downloader = self.client.GetReportDownloader(version='v201809')
         self.__init_brief()
         self.__init_report()
+        self.__init_predicates_object()
         
     def __init_customer(self, media):
         global database
@@ -116,6 +120,14 @@ class ReportGenerator(object):
     def __init_report(self):
         self.report = Report()
         
+    def _init_fields(self):
+        self.fields = self._fields.copy()
+        self.columns = self._columns.copy()
+        
+    def __init_predicates_object(self):
+        self.report.selector_object.predicates_object.spec['field'] = 'CampaignId'
+        self.report.selector_object.predicates_object.spec['values'] = self.campaign_id
+        
     def __init_selector(self, date_preset=None):
         self.date_preset = date_preset
         if not date_preset or date_preset == 'ALL_TIME':
@@ -124,8 +136,38 @@ class ReportGenerator(object):
         else:
             self.report.selector_object.spec['dateRange']['min'] = datetime.date.today().strftime("%Y%m%d")
             self.report.selector_object.spec['dateRange']['max'] = datetime.date.today().strftime("%Y%m%d")
+        self.report.selector_object.spec['fields'] = self.fields
+            
+    def _extract_value(self, value):
+        if value == 'day':
+            self.fields.append('Date')
+            self.columns.append('Date')
+        elif value == 'hour':
+            self.fields.append('HourOfDay')
+            self.columns.append('hour_of_day')
+            # breakdown 'hour' is not compatible with 'AllConversions' & 'CostPerAllConversion'
+            self.fields.remove('AllConversions'), self.fields.remove('CostPerAllConversion')
+            self.columns.remove('all_conversions'), self.columns.remove('cost_per_all_conversion')
+        elif value == 'device':
+            self.fields.append('Device')
+            self.columns.append('device')
+            
+    def add_param(self, key, value):
+        if key in self.param_types.keys():
+            if value in self.param_types[key]:
+                self._extract_value(value)
+        return self
+            
+    def add_params(self, params):
+        self._init_fields()
+        if params is None:
+            return self
+        for key in params.keys():
+            self.add_param(key, params[key])
+        return self
     
-    def get_insights(self, date_preset):
+    def get_insights(self, date_preset, params=None):
+        self.add_params(params)
         self.__init_selector(date_preset)
         data = self.report_downloader.DownloadReportAsString(self.report.spec,
                                                              skip_report_header=True,
@@ -134,11 +176,12 @@ class ReportGenerator(object):
                                                              include_zero_impressions=True,
                                                              client_customer_id=self.customer_id)
         data_list = data.split('\n')[:-1]
-        data_df = pd.DataFrame( columns=self._fields, data=[data.split(',') for data in data_list] )
-        data_df['Ctr'] = data_df.Ctr.str.split('%', expand = True)[0]
+        data_df = pd.DataFrame( columns=self.fields, data=[data.split(',') for data in data_list] )
+        if 'Ctr' in data_df.columns:
+            data_df['Ctr'] = data_df.Ctr.str.split('%', expand = True)[0]
         data_df[data_df.columns.difference( self._non_numeric_columns )] = data_df[data_df.columns.difference( self._non_numeric_columns )].apply(pd.to_numeric, errors='ignore')
         data_df[data_df.columns.intersection( self._money_columns )] = data_df[data_df.columns.intersection( self._money_columns )].div(1000000)
-        data_df.rename( columns=dict(zip(data_df.columns, self._columns)), inplace=True)
+        data_df.rename( columns=dict(zip(data_df.columns, self.columns)), inplace=True)
         return [data.to_dict() for idx, data in data_df.iterrows()]
 
 
@@ -161,17 +204,42 @@ class CampaignReportGenerator(ReportGenerator):
     
     def __init__(self, campaign_id, media):
         super().__init__(campaign_id, media)
+        self.fields = CampaignReportGenerator._fields.copy()
+        self.columns = CampaignReportGenerator._columns.copy()
         self.__init_report()
-        self.__init__selector_fields()
-        
-    def __init__selector_fields(self):
-        self.report.selector_object.spec['fields'] = self._fields
-        self.report.selector_object.predicates_object.spec['field'] = 'CampaignId'
-        self.report.selector_object.predicates_object.spec['values'] = self.campaign_id
+#         self.__init_predicates_object()
         
     def __init_report(self):
         self.report.spec[self.report.report_name] = self.report_name
         self.report.spec[self.report.report_type] = self.report_type
+
+
+# In[ ]:
+
+
+customer_id = 8845038097
+campaign_id = 2080506438
+
+
+# In[ ]:
+
+
+c = CampaignReportGenerator(campaign_id, media='gdn')
+
+
+# In[ ]:
+
+
+params = {
+    'breakdowns': 'day'
+}
+data = c.get_insights(date_preset=None, params=params)
+
+
+# In[ ]:
+
+
+pd.DataFrame(data=data)
 
 
 # In[ ]:
@@ -193,13 +261,9 @@ class AdGroupReportGenerator(ReportGenerator):
     
     def __init__(self, campaign_id, media):
         super().__init__(campaign_id, media)
+        self._fields = self._fields
+        self._columns = self._columns
         self.__init_report()
-        self.__init__selector_fields()
-        
-    def __init__selector_fields(self):
-        self.report.selector_object.spec['fields'] = self._fields
-        self.report.selector_object.predicates_object.spec['field'] = 'CampaignId'
-        self.report.selector_object.predicates_object.spec['values'] = self.campaign_id
         
     def __init_report(self):
         self.report.spec[self.report.report_name] = self.report_name
@@ -225,13 +289,9 @@ class AudienceReportGenerator(ReportGenerator):
     
     def __init__(self, campaign_id, media):
         super().__init__(campaign_id, media)
+        self._fields = self._fields
+        self._columns = self._columns
         self.__init_report()
-        self.__init__selector_fields()
-        
-    def __init__selector_fields(self):
-        self.report.selector_object.spec['fields'] = self._fields
-        self.report.selector_object.predicates_object.spec['field'] = 'CampaignId'
-        self.report.selector_object.predicates_object.spec['values'] = self.campaign_id
         
     def __init_report(self):
         self.report.spec[self.report.report_name] = self.report_name
@@ -257,13 +317,9 @@ class DisplayTopicReportGenerator(ReportGenerator):
     
     def __init__(self, campaign_id, media):
         super().__init__(campaign_id, media)
+        self._fields = self._fields
+        self._columns = self._columns
         self.__init_report()
-        self.__init__selector_fields()
-        
-    def __init__selector_fields(self):
-        self.report.selector_object.spec['fields'] = self._fields
-        self.report.selector_object.predicates_object.spec['field'] = 'CampaignId'
-        self.report.selector_object.predicates_object.spec['values'] = self.campaign_id
         
     def __init_report(self):
         self.report.spec[self.report.report_name] = self.report_name
@@ -289,13 +345,9 @@ class DisplayKeywordReportGenerator(ReportGenerator):
     
     def __init__(self, campaign_id, media):
         super().__init__(campaign_id, media)
+        self._fields = self._fields
+        self._columns = self._columns
         self.__init_report()
-        self.__init__selector_fields()
-        
-    def __init__selector_fields(self):
-        self.report.selector_object.spec['fields'] = self._fields
-        self.report.selector_object.predicates_object.spec['field'] = 'CampaignId'
-        self.report.selector_object.predicates_object.spec['values'] = self.campaign_id
         
     def __init_report(self):
         self.report.spec[self.report.report_name] = self.report_name
@@ -321,13 +373,9 @@ class UrlReportGenerator(ReportGenerator):
     
     def __init__(self, campaign_id, media):
         super().__init__(campaign_id, media)
+        self.fields = self._fields
+        self.columns = self._columns
         self.__init_report()
-        self.__init__selector_fields()
-        
-    def __init__selector_fields(self):
-        self.report.selector_object.spec['fields'] = self._fields
-        self.report.selector_object.predicates_object.spec['field'] = 'CampaignId'
-        self.report.selector_object.predicates_object.spec['values'] = self.campaign_id
         
     def __init_report(self):
         self.report.spec[self.report.report_name] = self.report_name
@@ -343,11 +391,75 @@ class UrlReportGenerator(ReportGenerator):
                                                              client_customer_id=self.customer_id)
         data_list = data.split('\n')[:-1]
         data_df = pd.DataFrame( columns=self._fields, data=[data.split(',') for data in data_list] )
-#         data_df['Ctr'] = data_df.Ctr.str.split('%', expand = True)[0]
+        if 'Ctr' in data_df.columns:
+            data_df['Ctr'] = data_df.Ctr.str.split('%', expand = True)[0]
         data_df[data_df.columns.difference( self._non_numeric_columns )] = data_df[data_df.columns.difference( self._non_numeric_columns )].apply(pd.to_numeric, errors='ignore')
         data_df[data_df.columns.intersection( self._money_columns )] = data_df[data_df.columns.intersection( self._money_columns )].div(1000000)
-#         data_df.rename( columns=dict(zip(data_df.columns, self._columns)), inplace=True)
-        return data_df
+        data_df = data_df.groupby('DisplayName').agg({'ExternalCustomerId': lambda x: x.mean(),
+                                                      'CampaignId': lambda x: x.mean(),
+                                                      'Cost': 'sum',
+                                                      'Impressions': 'sum',
+                                                      'Clicks': 'sum',
+                                                      'AllConversions': 'sum',
+                                                      'Conversions': 'sum',
+                                                       'ViewThroughConversions': 'sum',
+                                                      'Ctr': lambda x: (data_df.loc[x.index].Clicks.sum())/(data_df.loc[x.index].Impressions.sum())*100,
+                                                     }).reset_index()
+        data_df.rename( columns=dict(zip(self._fields, self._columns)), inplace=True)
+        return [data.to_dict() for idx, data in data_df.iterrows()]
+
+
+# In[ ]:
+
+
+class PlacementReportGenerator(ReportGenerator):
+    _fields = [
+        'ExternalCustomerId', 'CampaignId', 'DisplayName', 'Cost', 'Ctr', 'Impressions', 'Clicks', 'AllConversions',
+        'Conversions', 'ViewThroughConversions', 'AverageCpc', 'CostPerConversion', 'CostPerAllConversion']
+    _columns = [
+        'customer_id', 'campaign_id', 'display_name', 'spend', 'ctr', 'impressions', 'clicks', 'all_conversions',
+        'conversions', 'view_conversions', 'cost_per_click', 'cost_per_conversion', 'cost_per_all_conversion',
+    ]
+    report_name = 'PLACEMENT_PERFORMANCE_REPORT'
+    report_type = 'PLACEMENT_PERFORMANCE_REPORT'
+    
+    def __init__(self, campaign_id, media):
+        super().__init__(campaign_id, media)
+        self.__init_report()
+        self.fields = self._fields.copy()
+        self.columns = self._columns.copy()
+        
+    def __init_report(self):
+        self.report.spec[self.report.report_name] = self.report_name
+        self.report.spec[self.report.report_type] = self.report_type
+        
+    def get_insights(self, date_preset):
+        self._ReportGenerator__init_selector(date_preset)
+        data = self.report_downloader.DownloadReportAsString(self.report.spec,
+                                                             skip_report_header=True,
+                                                             skip_column_header=True,
+                                                             skip_report_summary=True,
+                                                             include_zero_impressions=False,
+                                                             client_customer_id=self.customer_id)
+        data_list = data.split('\n')[:-1]
+        data_df = pd.DataFrame( columns=self.fields, data=[data.split(',') for data in data_list] )
+        if 'Ctr' in data_df.columns:
+            data_df['Ctr'] = data_df.Ctr.str.split('%', expand = True)[0]
+        data_df[data_df.columns.difference( self._non_numeric_columns )] = data_df[
+            data_df.columns.difference( self._non_numeric_columns )].apply(pd.to_numeric, errors='ignore')
+        data_df[data_df.columns.intersection( self._money_columns )] = data_df[
+            data_df.columns.intersection( self._money_columns )].div(1000000)
+        data_df = data_df.groupby('DisplayName').agg({'ExternalCustomerId': lambda x: x.mean(),
+                                                      'CampaignId': lambda x: x.mean(),
+                                                      'Cost': 'sum',
+                                                      'Impressions': 'sum',
+                                                      'Clicks': 'sum',
+                                                      'AllConversions': 'sum',
+                                                      'Conversions': 'sum',
+                                                       'ViewThroughConversions': 'sum',
+                                                      'Ctr': lambda x: (data_df.loc[x.index].Clicks.sum())/(data_df.loc[x.index].Impressions.sum())*100,
+                                                     }).reset_index()
+        data_df.rename( columns=dict(zip(self._fields, self._columns)), inplace=True)
         return [data.to_dict() for idx, data in data_df.iterrows()]
 
 
@@ -370,13 +482,9 @@ class SearchKeywordReportGenerator(ReportGenerator):
     
     def __init__(self, campaign_id, media):
         super().__init__(campaign_id, media)
+        self.fields = self._fields.copy()
+        self.columns = self._columns.copy()
         self.__init_report()
-        self.__init__selector_fields()
-        
-    def __init__selector_fields(self):
-        self.report.selector_object.spec['fields'] = self._fields
-        self.report.selector_object.predicates_object.spec['field'] = 'CampaignId'
-        self.report.selector_object.predicates_object.spec['values'] = self.campaign_id
         
     def __init_report(self):
         self.report.spec[self.report.report_name] = self.report_name
@@ -396,63 +504,6 @@ class SearchKeywordReportGenerator(ReportGenerator):
             data_df.columns.difference( self._non_numeric_columns )].apply(pd.to_numeric, errors='ignore')
         data_df[data_df.columns.intersection( self._money_columns )] = data_df[
             data_df.columns.intersection( self._money_columns )].div(1000000)
-        data_df.rename( columns=dict(zip(self._fields, self._columns)), inplace=True)
-        return [data.to_dict() for idx, data in data_df.iterrows()]
-
-
-# In[ ]:
-
-
-class PlacementReportGenerator(ReportGenerator):
-    _fields = [
-        'ExternalCustomerId', 'CampaignId', 'DisplayName', 'Cost', 'Ctr', 'Impressions', 'Clicks', 'AllConversions',
-        'Conversions', 'ViewThroughConversions', 'AverageCpc', 'CostPerConversion', 'CostPerAllConversion']
-    _columns = [
-        'customer_id', 'campaign_id', 'display_name', 'spend', 'ctr', 'impressions', 'clicks', 'all_conversions',
-        'conversions', 'view_conversions', 'cost_per_click', 'cost_per_conversion', 'cost_per_all_conversion',
-    ]
-    report_name = 'PLACEMENT_PERFORMANCE_REPORT'
-    report_type = 'PLACEMENT_PERFORMANCE_REPORT'
-    
-    def __init__(self, campaign_id, media):
-        super().__init__(campaign_id, media)
-        self.__init_report()
-        self.__init__selector_fields()
-        
-    def __init__selector_fields(self):
-        self.report.selector_object.spec['fields'] = self._fields
-        self.report.selector_object.predicates_object.spec['field'] = 'CampaignId'
-        self.report.selector_object.predicates_object.spec['values'] = self.campaign_id
-        
-    def __init_report(self):
-        self.report.spec[self.report.report_name] = self.report_name
-        self.report.spec[self.report.report_type] = self.report_type
-        
-    def get_insights(self, date_preset):
-        self._ReportGenerator__init_selector(date_preset)
-        data = self.report_downloader.DownloadReportAsString(self.report.spec,
-                                                             skip_report_header=True,
-                                                             skip_column_header=True,
-                                                             skip_report_summary=True,
-                                                             include_zero_impressions=False,
-                                                             client_customer_id=self.customer_id)
-        data_list = data.split('\n')[:-1]
-        data_df = pd.DataFrame( columns=self._fields, data=[data.split(',') for data in data_list] )
-        data_df['Ctr'] = data_df.Ctr.str.split('%', expand = True)[0]
-        data_df[data_df.columns.difference( self._non_numeric_columns )] = data_df[
-            data_df.columns.difference( self._non_numeric_columns )].apply(pd.to_numeric, errors='ignore')
-        data_df[data_df.columns.intersection( self._money_columns )] = data_df[
-            data_df.columns.intersection( self._money_columns )].div(1000000)
-        data_df = data_df.groupby('DisplayName').agg({'ExternalCustomerId': lambda x: x.mean(),
-                                                      'CampaignId': lambda x: x.mean(),
-                                                      'Cost': 'sum',
-                                                      'Impressions': 'sum',
-                                                      'Clicks': 'sum',
-                                                      'AllConversions': 'sum',
-                                                      'Conversions': 'sum',
-                                                       'ViewThroughConversions': 'sum',
-                                                      'Ctr': lambda x: (data_df.loc[x.index].Clicks.sum())/(data_df.loc[x.index].Impressions.sum())*100,
-                                                     }).reset_index()
         data_df.rename( columns=dict(zip(self._fields, self._columns)), inplace=True)
         return [data.to_dict() for idx, data in data_df.iterrows()]
 
